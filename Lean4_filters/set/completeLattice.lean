ec36b244643c940d8200bf8dfc623c70fa95873a
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-03-24 00:20:36 +0000

    part way through proof set is a complete lattice

--- Lean4_filters/set/completeLattice.lean
+++ Lean4_filters/set/completeLattice.lean
@@ -4,4 +4,21 @@ import Lean4_filters.lattice.completeLattice
 
 variable {α : Type u}
 
-instance : CompleteLattice (Set α) where
+#check True
+
+instance : CompleteLattice (Set α) := {
+  top := Set.univ
+  leTop := by
+    intro s x hx;
+    trivial;
+  bot := Set.empty
+  botLe := by
+    intro s x hx;
+    cases hx;
+  supr := λ C x => ∃ s, s ∈ C ∧ x ∈ s   
+  leSupr := sorry
+  suprLe := sorry
+  infi := λ C x => ∀ s, s ∈ C ∧ x ∈ s   
+  infiLe := sorry
+  leInfi := sorry
+}
