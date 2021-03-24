cb102b8be7277c01446cb8126e523bb47539a703
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-03-24 00:21:09 +0000

    add cases with and trivial tactics

--- Lean4_filters/tactics/basic.lean
+++ Lean4_filters/tactics/basic.lean
@@ -13,13 +13,13 @@ syntax "right" : tactic
 macro_rules
   | `(tactic| right) => `(tactic| apply Or.inr)
 
-syntax "split" : tactic
-
 /-
 
 ## `split`
 
 -/
+
+syntax "split" : tactic
 
 macro_rules
   | `(tactic| split) => `(tactic| apply And.intro)
@@ -36,3 +36,13 @@ syntax "cases" term "with" ident withPosition(ident) : tactic
 macro_rules
   | `(tactic| cases $d:term with $p:ident $q:ident) => `(tactic| cases $d:term with | _ $p $q => ?_)
 
+/-
+
+## `trivial`
+
+-/
+
+syntax "trivial" : tactic
+
+macro_rules
+  | `(tactic| trivial) => `(tactic| exact True.intro)
