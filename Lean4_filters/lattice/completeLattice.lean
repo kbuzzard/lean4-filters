659ba9ed1324118d2a996068c6cf72b0ca962a1b
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2021-03-24 00:20:24 +0000

    move HasBot / HasTop

--- Lean4_filters/lattice/completeLattice.lean
+++ Lean4_filters/lattice/completeLattice.lean
@@ -1,12 +1,22 @@
 import Lean4_filters.tactics.basic
 import Lean4_filters.set.basic
 
+class HasTop (P : Type u) where
+  top : P
+
+notation "⊤" => HasTop.top
+
+class HasBot (P : Type u) where
+  bot : P
+
+notation "⊥" => HasBot.bot
+
 class CompleteLattice (P : Type u) extends Lattice P, HasTop P, HasBot P where
   leTop : ∀ (a : P), a ≤ ⊤
   botLe : ∀ (a : P), ⊥ ≤ a
-  Supr : Set P → P
-  Infi : Set P → P
-  leSupr : ∀ (s : Set P) (a : P), a ∈ s → a ≤ Supr s
-  suprLe : ∀ (s : Set P) (a : P), (∀ (b : P), b ∈ s → b ≤ a) → Supr s ≤ a
-  InfiLe : ∀ (s : Set P) (a : P), a ∈ s → Infi s ≤ a
-  LeInfi : ∀ (s : Set P) (a : P), (∀ (b : P), b ∈ s → a ≤ b) → a ≤ Infi s
+  supr : Set P → P
+  infi : Set P → P
+  leSupr : ∀ (s : Set P) (a : P), a ∈ s → a ≤ supr s
+  suprLe : ∀ (s : Set P) (a : P), (∀ (b : P), b ∈ s → b ≤ a) → supr s ≤ a
+  infiLe : ∀ (s : Set P) (a : P), a ∈ s → infi s ≤ a
+  leInfi : ∀ (s : Set P) (a : P), (∀ (b : P), b ∈ s → a ≤ b) → a ≤ infi s
