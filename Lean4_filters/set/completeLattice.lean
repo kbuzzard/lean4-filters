118de921eff0dd47b3707fc5a374b5384da13baa
Yakov Pechersky <ffxen158@gmail.com>
2021-03-24 04:43:38 -0400

    remove variable line

--- Lean4_filters/set/completeLattice.lean
+++ Lean4_filters/set/completeLattice.lean
@@ -2,8 +2,6 @@
 -- unknown package Lean4_filters
 import Lean4_filters.lattice.completeLattice
 
-variable {α : Type u}
-
 instance : CompleteLattice (Set α) := {
   top := Set.univ
   leTop := by
@@ -13,10 +11,10 @@ instance : CompleteLattice (Set α) := {
   botLe := by
     intro s x hx;
     cases hx;
-  supr := λ C x => ∃ s, s ∈ C ∧ x ∈ s   
+  supr := λ C x => ∃ s, s ∈ C ∧ x ∈ s
   leSupr := λ _ s hs _ hx => ⟨s, hs, hx⟩ -- why doesn't `supr` appear here, but instead have the unfolded defn?
   suprLe := λ C s hs x ⟨t, ht, hx⟩ => hs t ht hx
   infi := λ C x => ∀ s, s ∈ C → x ∈ s
-  infiLe := λ C s hs x h => h s hs 
+  infiLe := λ C s hs x h => h s hs
   leInfi := λ C s h x hx t ht => h t ht hx
 }
